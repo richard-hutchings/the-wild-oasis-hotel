dce09b3fa82d596125c3c49edb05e8df08d8bc85
richard-hutchings <40692203+richard-hutchings@users.noreply.github.com>
2024-09-09 16:46:24 +1000

    test commit

---
+++ src/features/cabins/CabinTableOperations.jsx
@@ -0,0 +1,33 @@
+import TableOperations from "../../ui/TableOperations";
+import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
+
+function CabinTableOperations() {
+  return (
+    <TableOperations>
+      <Filter
+        filterField="discount"
+        options={[
+          { value: "all", label: "all" },
+          { value: "no-discount", label: "no discount" },
+          { value: "with-discount", label: "with discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "sort by name (A-Z)" },
+          { value: "name-desc", label: "sort by name (Z-A)" },
+          { value: "regularPrice-asc", label: "sort by price (low first)" },
+          { value: "regularPrice-desc", label: "sort by price (high first)" },
+          { value: "maxCapacity-asc", label: "sort by capacity (low first)" },
+          {
+            value: "maxCapacity-desc",
+            label: "sort by cacpacity (high first)",
+          },
+        ]}
+      />
+    </TableOperations>
+  );
+}
+
+export default CabinTableOperations;
