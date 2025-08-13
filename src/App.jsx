972f17c244b30177f003361fbbf746d9c13b754c
Richard <40692203+richard-hutchings@users.noreply.github.com>
2025-08-13 16:48:30 +1000

    testing login

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
-import Login from "./pages/Login";
+// import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import ProtectedRoute from "./ui/ProtectedRoute";
@@ -52,7 +52,7 @@ function App() {
                 <Route path="settings" element={<Settings />} />
                 <Route path="account" element={<Account />} />
               </Route>
-              <Route path="login" element={<Login />} />
+{/*               <Route path="login" element={<Login />} /> */}
               <Route path="*" element={<PageNotFound />} />
             </Routes>
           </BrowserRouter>
